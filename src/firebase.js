40e85ec77fb8701dfb74b4b1034c59d450d7367b
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-08-18 08:40:00 +0100

    cleanup-complete

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBF_FPKdV7RQabyTYJlL5sugw-KcDd4r2c",
+  authDomain: "tiktok-clone-269b5.firebaseapp.com",
+  projectId: "tiktok-clone-269b5",
+  storageBucket: "tiktok-clone-269b5.appspot.com",
+  messagingSenderId: "96395459882",
+  appId: "1:96395459882:web:e4aef1c167a085620b7253",
+  measurementId: "G-NDEC3V8TFX",
+};
